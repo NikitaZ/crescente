3458f09d0f8202e88839e40ef7afa115570929a3
Nikita Zinoviev <nikita.zinoviev@gmail.com>
2022-05-04 20:57:00 +0300

    Added simple application scoped JSF bean which allows to indicate when in development mode.

---
+++ src/main/java/com/example/mydemofullweb/web/ServiceBean.java
@@ -0,0 +1,26 @@
+package com.example.mydemofullweb.web;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.application.ProjectStage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+/**
+ * Application scope data bean for the application.
+ * An instance of this class is created automatically, the first
+ * time application evaluates a value binding expression or method binding
+ * expression that references a managed bean using this class.
+ */
+@Named("ServiceBean")
+@ApplicationScoped
+public class ServiceBean {
+
+//     this is a bit more performant than
+//    private UserService userManager = new UserServiceClient();
+//    @EJB
+//    private UserManagerLocal userManager;
+
+    public boolean getDevelopment() {
+        return FacesContext.getCurrentInstance().getApplication().getProjectStage() == ProjectStage.Development;
+    }
+}
