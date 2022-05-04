d89611e33137a0525b5185634ca890a6facc8c01
Nikita Zinoviev <nikita.zinoviev@gmail.com>
2022-05-04 21:57:47 +0300

    Switched from standard Application to ResourceConfig to make sure rest api is available at /api, not just /faces/api.

--- src/main/java/com/example/mydemofullweb/HelloApplication.java
+++ src/main/java/com/example/mydemofullweb/HelloApplication.java
@@ -1,9 +1,45 @@
 package com.example.mydemofullweb;
 
 import jakarta.ws.rs.ApplicationPath;
+import org.glassfish.jersey.message.GZipEncoder;
 import jakarta.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
+
+import java.util.logging.Logger;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
 
 }
+//public class HelloApplication extends ResourceConfig {
+//
+//    private static final Logger LOGGER = Logger.getLogger(HelloApplication.class.getName());
+//
+//    public HelloApplication() {
+//
+//        LOGGER.info("Configuration Hello REST Application");
+//
+////        LOGGER.info("Register Jackson");
+////        register(JacksonFeature.class);
+//
+////        LOGGER.info("Register PuzzleJSONMapper");
+////        register(PuzzleJSONMapper.class);
+//
+////        disabled to avoid extra logging
+////        LOGGER.info("Register UserCookieFilter");
+////        register(UserCookieFilter.class);
+////        register(UserAuthenticationFilter.class);
+//
+//        LOGGER.info("Register packages");
+//        packages("com.example.mydemofullweb", "com.example.mydemofullweb.web", "com.example.mydemofullweb.web.service");
+//
+//        LOGGER.info("Enabling GZip filter");
+//        EncodingFilter.enableFor(this, GZipEncoder.class);
+//
+//        // Enable for debugging:
+//        //LOGGER.info("Enable logging");
+//        //registerInstances(new LoggingFilter(LOGGER, true));
+//    }
+//
+//}
