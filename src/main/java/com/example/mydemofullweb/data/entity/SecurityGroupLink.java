2d674feee4aa6868c6ba803384cd5c1d32d2eed7
Nikita Zinoviev <nikita.zinoviev@gmail.com>
2022-05-07 18:38:09 +0300

    Added unique constraint on {"groupName", "userAccountName"} to SecurityGroupLink.

--- src/main/java/com/example/mydemofullweb/data/entity/SecurityGroupLink.java
+++ src/main/java/com/example/mydemofullweb/data/entity/SecurityGroupLink.java
@@ -16,6 +16,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import java.io.Serializable;
 
@@ -24,6 +26,8 @@ import java.io.Serializable;
  * See Group(s) in Duke’s Forest Case Study Example from JakartaEE tutorial for a many-to-many example.
  */
 @Entity
+
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"groupName", "userAccountName"}))
 public class SecurityGroupLink implements Serializable {
 
     @Id
